a95543819d19b8f91b35b5c984d9d7b0880606e3
Steven Delcourt <stv@mentiblue.com>
2026-05-28 19:34:55 +0200

    Mobile: text-only logo + sidebar redesign

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,13 +79,8 @@ function Sidebar({ session }: { session: { id: number; email: string; role: stri
     <aside className="sidebar">
       <div className="sidebar-logo">
         <a href="/">
-          <Image
-            src="/lepharo.svg"
-            alt="Le Pharo"
-            width={223}
-            height={273}
-            className="sidebar-building-icon"
-          />
+          <Image src="/lepharo.svg" alt="Le Pharo" width={223} height={273} className="sidebar-building-icon" />
+          <Image src="/lepharo-text.svg" alt="Le Pharo" width={140} height={44} className="sidebar-text-icon" />
         </a>
       </div>
 
